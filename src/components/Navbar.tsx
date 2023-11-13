924a353a9bb5c9c4b59b02d5b45f9ea942bf4ea1
theFutureGuy <ashiqgit789@gmail.com>
2023-11-13 20:46:42 +0530

    Home page design base with setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+    return(
+        <div className="fixed-top-0 inset-x-0 h-fit">
+            Navbar
+        </div>
+    );
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,6 @@ import { ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import { formatDistanceToNowStrict } from 'date-fns'
 import locale from 'date-fns/locale/en-US'
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
